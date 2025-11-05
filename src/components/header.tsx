8189bed7c307b23485ee91b8b4f977b572bb7c9f
spectralPA1 <spectralpa@gmail.com>
2025-11-05 10:00:39 +0500

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,54 @@
+
+"use client";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
+
+export const AcmeLogo = () => {
+  return (
+    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
+      <path
+        clipRule="evenodd"
+        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default function Header() {
+
+  const navItems =[
+    { label: "Home", href: "/" },
+    { label: "Features", href: "/" },
+    { label: "Pricing", href: "/" },
+    { label: "About", href: "/" },
+  ]
+
+  
+
+  return (
+    <Navbar shouldHideOnScroll>
+      <NavbarBrand>
+        <AcmeLogo />
+        <p className="font-bold text-inherit">EFT</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {navItems.map((item) => (
+          <NavbarItem key={item.label}>
+            <Link href={item.href}>{item.label}</Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
